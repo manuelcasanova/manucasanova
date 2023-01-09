1dec2196b7bab801d9b4b5f2f0ae77d5a3970d44
Manuel Casanova <manucasanova@hotmail.com>
2023-01-09 06:28:49 -0800

    Add information to the list hospitality

--- client/src/components/Hospitality.jsx
+++ client/src/components/Hospitality.jsx
@@ -6,10 +6,52 @@ export default function Hospitality() {
       <div className="hospitality-section">
         <div className="hospitality-section-title">Experience</div>
         <div className="list">
-        <ul>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
+          <ul>
+            <li>
+              <div className="li-title">2022 - present</div>
+              <div>Rocky Mountain Flatbread (Vancouver, B.C.)</div>
+              <div>Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2018 - 2021</div>
+              <div>Rice 'n Spice (Vancouver, B.C.)</div>
+              <div>Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2014 - 2015</div>
+              <div>Breathless Punta Cana (Dominican Republic)</div>
+              <div>Assistant Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2012 - 2014</div>
+              <div>NH Haiti El Rancho (Port-au-Prince, Haiti)</div>
+              <div>General Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2011 - 2012</div>
+              <div>Now Larimar Punta Cana (Dominican Republic)</div>
+              <div>Resident Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2009 - 2011</div>
+              <div>Gran Bahía Príncipe Cayo Levantado (Dominican Republic)</div>
+              <div>Resident Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2006 - 2009</div>
+              <div>Gran Bahía Príncipe El Portillo (Dominican Republic)</div>
+              <div>Resident Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2004 - 2006</div>
+              <div>Luperón Beach Resort (Dominican Republic)</div>
+              <div>Junior Assistant Manager</div>
+            </li>
+            <li>
+              <div className="li-title">2001 - 2004</div>
+              <div>Hotel Gran Sol (Barcelona, Spain)</div>
+              <div>Hotel-school student</div>
+            </li>
 
           </ul>
         </div>
@@ -18,19 +60,50 @@ export default function Hospitality() {
       <div className="hospitality-section">
         <div className="hospitality-section-title">Skills</div>
         <div className="list">
-        <ul>
-            <li className="li-title">Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-            <li className="li-title">Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk</li>
+          <ul>
+
+
+            <div className="li-title">Industry specific</div>
+            <li>Hotel management</li>
+            <li>Restaurant management</li>
+            <li>Event management</li>
+            <li>Rooms Division management</li>
+            <li>Front desk</li>
+            <li>Housekeeping management</li>
+            <li>Food safety</li>
+            <li>Quality control</li>
+            <li>Customer service</li>
+            <li>Problem solving</li>
+            <li>Recordkeeping</li>
+            <li>Human resources management</li>
+            <li>Financial statements analysis</li>
+            <li>Preopening</li>
+            <li>Budgets</li>
+            <li>Attention to detail</li>
+
+            <div className="li-title">Informatics</div>
+            <li>Touch Bistro</li>
+            <li>POS systems</li>
+            <li>Micros-Fidelio</li>
+            <li>Hotel-link</li>
+            <li>ACI Hotel</li>
+            <li>New Hotel</li>
+            <li>NH Hoteld system</li>
+            <li>Innsist</li>
+            <li>Web development: HTML, CSS, ReactJS, NodeJS, ExpressJS, PostgreSQL</li>
+
+            <div className="li-title">Office software</div>
+            <li>Word processing</li>
+            <li>Spreadsheets</li>
+            <li>Slide presentation software</li>
+            <li>Email management software</li>
+
+            <div className="li-title">Languages</div>
+            <li>Spanish: native</li>
+            <li>English: full professional proficiency</li>
+            <li>French: professional proficiency</li>
+
+
           </ul>
         </div>
 
@@ -40,9 +113,44 @@ export default function Hospitality() {
         <div className="hospitality-section-title">Degrees and courses</div>
         <div className="list">
           <ul>
-        
-            <li>Tea</li>
-            <li>Milk</li>
+
+            <li>
+              <div className="li-title">2001 - 2004</div>
+              <div>Tourist and hotel management college diploma</div>
+              <div>EUHT StPol - University of Girona (Barcelona, Spain)</div>
+            </li>
+            <li className="margin-top-2">Business organisation and management</li>
+            <li>Restaurant, catering and accomodation management</li>
+            <li>Analysis of financial statements</li>
+            <li>Human resources management</li>
+            <li>Financial mathematics</li>
+            <li>Tourism marketing</li>
+            <li>Restaurant marketing</li>
+            <li>Sommelier</li>
+            <li>Cocktail bar</li>
+            <li>New kitchen technologies</li>
+            <li>Revenue management</li>
+            <li>Kitchen: pantry, line, bakery</li>
+            <li>HACCP Principles and Application Guidelines</li>
+            <li>Public regulations in tourism</li>
+            <li>Introduction to restaurant management</li>
+            <li>Management skills for the hospitality industry</li>
+            <li>Introduction to law and private law</li>
+            <li>Introduction to economics</li>
+            <li>Accounting</li>
+            <li>Production operation and processes</li>
+            <li>Market structures</li>
+            <li>Regional tourism resources</li>
+            <li>Cultural heritage</li>
+            <li>World tourism geography</li>
+            <li>Qualitative and quantitative analysis techniques for tourism</li>
+            <li>Tourism information technology</li>
+            <li>Communication techniques applied to tourism</li>
+            <li>Structures and equipment</li>
+            <li>Information technologies</li>
+            <li>Museum and exhibit scheduling</li>
+            <li>Cultural tourism analysis</li>
+            <li>Amadeus</li>
           </ul>
         </div>
 
