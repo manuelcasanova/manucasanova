77309746b4b3693e348459ef1440915cb5760e57
Manuel Casanova <manucasanova@hotmail.com>
2023-01-09 05:30:20 -0800

    Prepare sections for experience skills and degrees. Style

--- client/src/components/Hospitality.jsx
+++ client/src/components/Hospitality.jsx
@@ -1,15 +1,58 @@
 import { Link } from "react-router-dom"
 
-export default function Hospitality () {
+export default function Hospitality() {
   return (
-    <>
     <div className="hospitality">
-      This section will soon be available!
-      
-    </div>
-    <div>
+      <div className="hospitality-section">
+        <div className="hospitality-section-title">Experience</div>
+        <div className="list">
+        <ul>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+
+          </ul>
+        </div>
+      </div>
+
+      <div className="hospitality-section">
+        <div className="hospitality-section-title">Skills</div>
+        <div className="list">
+        <ul>
+            <li className="li-title">Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+            <li className="li-title">Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk Milk</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div className="hospitality-section">
+        <div className="hospitality-section-title">Degrees and courses</div>
+        <div className="list">
+          <ul>
+        
+            <li>Tea</li>
+            <li>Milk</li>
+          </ul>
+        </div>
+
+      </div>
+
+
+
+      {/* <div>
       <Link to="/" className="no-decoration">Back home</Link>
+    </div> */}
     </div>
-    </>
   )
 }
